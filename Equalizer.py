456fb0936bfa2e809817c0a760611d0579483c04
TynClause <elvinnurfurqon17@gmail.com>
2020-03-25 17:57:20 +0700

    Update UI and add design

--- Equalizer.py
+++ Equalizer.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# by TynClause
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import librosa as lr
 import numpy as np
@@ -14,7 +10,6 @@ class Ui_Equalizer(object):
     fileName = None
 
     def setupUi(self, Equalizer):
-        self.UpdateFromSpinbox = False
         Equalizer.setObjectName("Equalizer")
         Equalizer.resize(796, 712)
         Equalizer.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
@@ -47,47 +42,31 @@ class Ui_Equalizer(object):
         self.C1.setOrientation(QtCore.Qt.Vertical)
         self.C1.setObjectName("C1")
         self.horizontalLayout.addWidget(self.C1)
-        # =======================================
         self.C1.setRange(0, 100)
         self.C1.valueChanged[int].connect(self.update_spinbox_C1)
-        # =======================================
         self.C2 = QtWidgets.QSlider(self.horizontalLayoutWidget)
         self.C2.setOrientation(QtCore.Qt.Vertical)
         self.C2.setObjectName("C2")
         self.horizontalLayout.addWidget(self.C2)
-        # =======================================
         self.C2.setRange(0, 100)
         self.C2.valueChanged[int].connect(self.update_spinbox_C2)
-        # =======================================
         self.C3 = QtWidgets.QSlider(self.horizontalLayoutWidget)
         self.C3.setOrientation(QtCore.Qt.Vertical)
         self.C3.setObjectName("C3")
         self.horizontalLayout.addWidget(self.C3)
-        # =======================================
         self.C3.setRange(0, 100)
         self.C3.valueChanged[int].connect(self.update_spinbox_C3)
-        # =======================================
         self.C4 = QtWidgets.QSlider(self.horizontalLayoutWidget)
         self.C4.setOrientation(QtCore.Qt.Vertical)
         self.C4.setObjectName("C4")
         self.horizontalLayout.addWidget(self.C4)
-        # =======================================
         self.C4.setRange(0, 100)
         self.C4.valueChanged[int].connect(self.update_spinbox_C4)
-        # =======================================
-        self.C5 = QtWidgets.QSlider(self.horizontalLayoutWidget)
-        self.C5.setOrientation(QtCore.Qt.Vertical)
-        self.C5.setObjectName("C5")
-        self.horizontalLayout.addWidget(self.C5)
-        # =======================================
-        self.C5.setRange(0, 100)
-        self.C5.valueChanged[int].connect(self.update_spinbox_C5)
-        # =======================================
         self.graphicsView = pg.PlotWidget(self.centralwidget)
         self.graphicsView.setGeometry(QtCore.QRect(90, 390, 621, 231))
         self.graphicsView.setObjectName("graphicsView")
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(110, 330, 51, 34))
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(130, 330, 73, 34))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(
             self.verticalLayoutWidget_2)
@@ -97,12 +76,8 @@ class Ui_Equalizer(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.verticalLayout_2.addWidget(self.label)
-        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_2.setObjectName("label_2")
-        self.verticalLayout_2.addWidget(self.label_2)
         self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(230, 330, 65, 34))
+        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(280, 330, 75, 34))
         self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
         self.verticalLayout_3 = QtWidgets.QVBoxLayout(
             self.verticalLayoutWidget_3)
@@ -112,11 +87,8 @@ class Ui_Equalizer(object):
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
         self.verticalLayout_3.addWidget(self.label_3)
-        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget_3)
-        self.label_4.setObjectName("label_4")
-        self.verticalLayout_3.addWidget(self.label_4)
         self.verticalLayoutWidget_4 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(360, 330, 71, 34))
+        self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(430, 330, 78, 34))
         self.verticalLayoutWidget_4.setObjectName("verticalLayoutWidget_4")
         self.verticalLayout_4 = QtWidgets.QVBoxLayout(
             self.verticalLayoutWidget_4)
@@ -126,11 +98,8 @@ class Ui_Equalizer(object):
         self.label_5.setAlignment(QtCore.Qt.AlignCenter)
         self.label_5.setObjectName("label_5")
         self.verticalLayout_4.addWidget(self.label_5)
-        self.label_6 = QtWidgets.QLabel(self.verticalLayoutWidget_4)
-        self.label_6.setObjectName("label_6")
-        self.verticalLayout_4.addWidget(self.label_6)
         self.verticalLayoutWidget_5 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_5.setGeometry(QtCore.QRect(490, 330, 77, 34))
+        self.verticalLayoutWidget_5.setGeometry(QtCore.QRect(590, 330, 78, 34))
         self.verticalLayoutWidget_5.setObjectName("verticalLayoutWidget_5")
         self.verticalLayout_5 = QtWidgets.QVBoxLayout(
             self.verticalLayoutWidget_5)
@@ -140,78 +109,44 @@ class Ui_Equalizer(object):
         self.label_7.setAlignment(QtCore.Qt.AlignCenter)
         self.label_7.setObjectName("label_7")
         self.verticalLayout_5.addWidget(self.label_7)
-        self.label_8 = QtWidgets.QLabel(self.verticalLayoutWidget_5)
-        self.label_8.setObjectName("label_8")
-        self.verticalLayout_5.addWidget(self.label_8)
-        self.verticalLayoutWidget_6 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_6.setGeometry(QtCore.QRect(620, 330, 75, 34))
-        self.verticalLayoutWidget_6.setObjectName("verticalLayoutWidget_6")
-        self.verticalLayout_6 = QtWidgets.QVBoxLayout(
-            self.verticalLayoutWidget_6)
-        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_6.setObjectName("verticalLayout_6")
-        self.label_9 = QtWidgets.QLabel(self.verticalLayoutWidget_6)
-        self.label_9.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_9.setObjectName("label_9")
-        self.verticalLayout_6.addWidget(self.label_9)
-        self.label_10 = QtWidgets.QLabel(self.verticalLayoutWidget_6)
-        self.label_10.setObjectName("label_10")
-        self.verticalLayout_6.addWidget(self.label_10)
         self.doubleSpinBox = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.doubleSpinBox.setGeometry(QtCore.QRect(110, 300, 62, 22))
+        self.doubleSpinBox.setGeometry(QtCore.QRect(130, 300, 62, 22))
         self.doubleSpinBox.setObjectName("doubleSpinBox")
-        # ========================================
         self.doubleSpinBox.setRange(0.0, 100.0)
         self.doubleSpinBox.setDecimals(2)
         self.doubleSpinBox.setSingleStep(0.1)
         self.doubleSpinBox.editingFinished.connect(
             self.update_slider_position_C1)
-        # ========================================
         self.doubleSpinBox_2 = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.doubleSpinBox_2.setGeometry(QtCore.QRect(230, 300, 62, 22))
+        self.doubleSpinBox_2.setGeometry(QtCore.QRect(290, 300, 62, 22))
         self.doubleSpinBox_2.setObjectName("doubleSpinBox_2")
-        # ========================================
         self.doubleSpinBox_2.setRange(0.0, 100.0)
         self.doubleSpinBox_2.setDecimals(2)
         self.doubleSpinBox_2.setSingleStep(0.1)
         self.doubleSpinBox_2.editingFinished.connect(
             self.update_slider_position_C2)
-        # ========================================
         self.doubleSpinBox_3 = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.doubleSpinBox_3.setGeometry(QtCore.QRect(370, 300, 62, 22))
+        self.doubleSpinBox_3.setGeometry(QtCore.QRect(440, 300, 62, 22))
         self.doubleSpinBox_3.setObjectName("doubleSpinBox_3")
-        # ========================================
         self.doubleSpinBox_3.setRange(0.0, 100.0)
         self.doubleSpinBox_3.setDecimals(2)
         self.doubleSpinBox_3.setSingleStep(0.1)
         self.doubleSpinBox_3.editingFinished.connect(
             self.update_slider_position_C3)
-        # ========================================
         self.doubleSpinBox_4 = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.doubleSpinBox_4.setGeometry(QtCore.QRect(500, 300, 62, 22))
+        self.doubleSpinBox_4.setGeometry(QtCore.QRect(600, 300, 62, 22))
         self.doubleSpinBox_4.setObjectName("doubleSpinBox_4")
-        # ========================================
         self.doubleSpinBox_4.setRange(0.0, 100.0)
         self.doubleSpinBox_4.setDecimals(2)
         self.doubleSpinBox_4.setSingleStep(0.1)
         self.doubleSpinBox_4.editingFinished.connect(
             self.update_slider_position_C4)
-        # ========================================
-        self.doubleSpinBox_5 = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.doubleSpinBox_5.setGeometry(QtCore.QRect(630, 300, 62, 22))
-        self.doubleSpinBox_5.setObjectName("doubleSpinBox_5")
-        # ========================================
-        self.doubleSpinBox_5.setRange(0.0, 100.0)
-        self.doubleSpinBox_5.setDecimals(2)
-        self.doubleSpinBox_5.setSingleStep(0.1)
-        self.doubleSpinBox_5.editingFinished.connect(
-            self.update_slider_position_C5)
-        # ========================================
         self.label_11 = QtWidgets.QLabel(self.centralwidget)
         self.label_11.setGeometry(QtCore.QRect(10, 490, 81, 16))
         self.label_11.setObjectName("label_11")
         self.label_12 = QtWidgets.QLabel(self.centralwidget)
         self.label_12.setGeometry(QtCore.QRect(360, 630, 81, 16))
+        self.label_12.setAlignment(QtCore.Qt.AlignCenter)
         self.label_12.setObjectName("label_12")
         self.label_13 = QtWidgets.QLabel(self.centralwidget)
         self.label_13.setGeometry(QtCore.QRect(330, 370, 151, 16))
@@ -219,6 +154,7 @@ class Ui_Equalizer(object):
         font.setBold(True)
         font.setWeight(75)
         self.label_13.setFont(font)
+        self.label_13.setAlignment(QtCore.Qt.AlignCenter)
         self.label_13.setObjectName("label_13")
         self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
         self.horizontalLayoutWidget_2.setGeometry(
@@ -267,9 +203,6 @@ class Ui_Equalizer(object):
     def update_spinbox_C4(self, value):
         self.doubleSpinBox_4.setValue(float(value)/100)
 
-    def update_spinbox_C5(self, value):
-        self.doubleSpinBox_5.setValue(float(value)/100)
-
     def update_slider_position_C1(self):
         self.UpdateFromSpinbox = True
         self.C1.setSliderPosition(self.doubleSpinBox.value()*100)
@@ -290,29 +223,17 @@ class Ui_Equalizer(object):
         self.C4.setSliderPosition(self.doubleSpinBox_4.value()*100)
         self.UpdateFromSpinbox = False
 
-    def update_slider_position_C5(self):
-        self.UpdateFromSpinbox = True
-        self.C5.setSliderPosition(self.doubleSpinBox_5.value()*100)
-        self.UpdateFromSpinbox = False
-
     def retranslateUi(self, Equalizer):
         _translate = QtCore.QCoreApplication.translate
         Equalizer.setWindowTitle(_translate("Equalizer", "MainWindow"))
         self.Judul.setText(_translate("Equalizer", "EQUALIZER"))
-        self.label.setText(_translate("Equalizer", "C1"))
-        self.label_2.setText(_translate("Equalizer", "Low"))
-        self.label_3.setText(_translate("Equalizer", "C2"))
-        self.label_4.setText(_translate("Equalizer", "Band 4-8 kHz"))
-        self.label_5.setText(_translate("Equalizer", "C3"))
-        self.label_6.setText(_translate("Equalizer", "Band 9-13 kHz"))
-        self.label_7.setText(_translate("Equalizer", "C4"))
-        self.label_8.setText(_translate("Equalizer", "Band 13-17 kHz"))
-        self.label_9.setText(_translate("Equalizer", "C5"))
-        self.label_10.setText(_translate("Equalizer", "High frequency"))
+        self.label.setText(_translate("Equalizer", "Low Pass Filter"))
+        self.label_3.setText(_translate("Equalizer", "High Pass Filter"))
+        self.label_5.setText(_translate("Equalizer", "Band Pass Filter"))
+        self.label_7.setText(_translate("Equalizer", "Band Stop Filter"))
         self.label_11.setText(_translate("Equalizer", "Magnitude [dB]"))
         self.label_12.setText(_translate("Equalizer", "Time (s)"))
-        self.label_13.setText(_translate(
-            "Equalizer", "vizualize equalizer audio"))
+        self.label_13.setText(_translate("Equalizer", "Visualisasi Audio"))
         self.pushButton.setText(_translate("Equalizer", "Load"))
         self.pushButton_2.setText(_translate("Equalizer", "Plot"))
         self.pushButton_3.setText(_translate("Equalizer", "Reset"))
